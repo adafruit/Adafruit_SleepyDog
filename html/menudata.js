a330ac2a7d22b6ea033056d238c453776d43295d
dherrada <dylan.herrada@gmail.com>
2020-06-12 15:33:36 -0400

    Moved to actions, a bit of doxy

---
+++ html/menudata.js
@@ -0,0 +1,7 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Variables",url:"globals_vars.html"}]}]}]}
---
+++ html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['adafruit_5fsleepydog_2ecpp',['Adafruit_SleepyDog.cpp',['../_adafruit___sleepy_dog_8cpp.html',1,'']]],
+  ['adafruit_5fsleepydog_2eh',['Adafruit_SleepyDog.h',['../_adafruit___sleepy_dog_8h.html',1,'']]],
+  ['adafruit_20sleepydog_20library',['Adafruit SleepyDog Library',['../index.html',1,'']]]
+];
