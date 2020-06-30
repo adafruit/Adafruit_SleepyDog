344bf9108479fc1c02e06806ccc6abfd9511cc01
hathach <thach@tinyusb.org>
2020-07-01 01:53:15 +0700

    fix nRF build with nrfx v2

--- utility/WatchdogNRF.cpp
+++ utility/WatchdogNRF.cpp
@@ -12,28 +12,28 @@ int WatchdogNRF::enable(int maxPeriodMS) {
 
   // cannot change wdt config register once it is started
   // return previous configured timeout
-  if (nrf_wdt_started())
+  if (nrf_wdt_started(NRF_WDT))
     return _wdto;
 
   // WDT run when CPU is sleep
-  nrf_wdt_behaviour_set(NRF_WDT_BEHAVIOUR_RUN_SLEEP);
-  nrf_wdt_reload_value_set((maxPeriodMS * 32768) / 1000);
+  nrf_wdt_behaviour_set(NRF_WDT, NRF_WDT_BEHAVIOUR_RUN_SLEEP);
+  nrf_wdt_reload_value_set(NRF_WDT, (maxPeriodMS * 32768) / 1000);
 
   // use channel 0
-  nrf_wdt_reload_request_enable(NRF_WDT_RR0);
+  nrf_wdt_reload_request_enable(NRF_WDT, NRF_WDT_RR0);
 
   // Start WDT
   // After started CRV, RREN and CONFIG is blocked
   // There is no way to stop/disable watchdog using source code
   // It can only be reset by WDT timeout, Pin reset, Power reset
-  nrf_wdt_task_trigger(NRF_WDT_TASK_START);
+  nrf_wdt_task_trigger(NRF_WDT, NRF_WDT_TASK_START);
 
   _wdto = maxPeriodMS;
 
   return maxPeriodMS;
 }
 
-void WatchdogNRF::reset() { nrf_wdt_reload_request_set(NRF_WDT_RR0); }
+void WatchdogNRF::reset() { nrf_wdt_reload_request_set(NRF_WDT, NRF_WDT_RR0); }
 
 // There is no way to stop/disable watchdog using source code
 void WatchdogNRF::disable() {}
