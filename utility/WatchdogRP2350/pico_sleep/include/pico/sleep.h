32f38a581e0b5b9edf0ef2122ffc04d1d5b7774f
Mikey Sklar <mikeysklar@gmail.com>
2026-03-23 10:08:04 -0700

    Fix RP2350 timer sleep support

--- utility/WatchdogRP2350/pico_sleep/include/pico/sleep.h
+++ utility/WatchdogRP2350/pico_sleep/include/pico/sleep.h
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#if defined(ARDUINO_ARCH_RP2350)
+#if defined(ARDUINO_ARCH_RP2350) || defined(PICO_RP2350) || defined(TARGET_RP2350)
 
 #ifndef _PICO_SLEEP_H_
 #define _PICO_SLEEP_H_
